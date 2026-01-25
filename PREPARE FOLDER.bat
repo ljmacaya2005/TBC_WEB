d96e570b7cec1c5ac31c574ed7f6cd4dddfccc4a
Lordy James Macaya <ljmacaya26@gmail.com>
2026-01-25 20:02:19 +0800

    Cleaned

---
+++ PREPARE FOLDER.bat
@@ -0,0 +1,26 @@
+@echo off
+cls
+cd /d "%~dp0"
+cd ..\..
+echo --- Initialize Git Repository ---
+git init
+
+:: Rename branch to 'main' to match modern GitHub standards
+git branch -M main
+
+cls
+echo Repository initialized.
+echo.
+
+echo Adding remote origin...
+:: The || echo... part handles cases where origin already exists
+git remote add origin "https://github.com/ljmacaya2005/TBC_WEB.git" || echo Remote 'origin' already exists.
+git fetch --all
+git reset --hard origin/main
+git clean -fd
+
+
+echo.
+echo Done!
+pause
+"OPERATIONS_MENU.bat"
